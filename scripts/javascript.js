30f6db7372f6d4d294bc108f810f3171b96ef8c9
FouadTaissate <112860051+FouadTaissate@users.noreply.github.com>
2023-01-24 00:40:21 +0100

    a

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -2,16 +2,15 @@ let slidesContainer = document.getElementById("slides-container");
 let slide = document.querySelector(".slide");
 let prevButton = document.getElementById("slide-arrow-prev");
 let nextButton = document.getElementById("slide-arrow-next");
-let plusProgress = document.getElementById("");
-
-nextButton.addEventListener("click", () => {
-  let slideWidth = slide.clientWidth;
-
-  slidesContainer.scrollLeft += slideWidth;
-});
 
 prevButton.addEventListener("click", () => {
   let slideWidth = slide.clientWidth;
 
   slidesContainer.scrollLeft -= slideWidth;
+});
+
+nextButton.addEventListener("click", () => {
+  let slideWidth = slide.clientWidth;
+
+  slidesContainer.scrollLeft += slideWidth;
 });
